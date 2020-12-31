5fba158b294ea507d420d7834f3bce211b6b0116
Elijah Steres <elijahsteres@gmail.com>
2020-12-31 17:56:08 -0500

    work on league filters

--- static/loader.js
+++ static/loader.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
     var socket = io.connect();
-    var gameslist = [];
+    var lastupdate;
     var grid = document.getElementById("container");
     
 
@@ -9,6 +9,41 @@ $(document).ready(function (){
     });
 
     socket.on("states_update", function (json) { //json is an object containing all game updates
+        lastupdate = json;
+        updateGames(json, $('#selected_filter').text());
+
+        //get all leagues
+        leagues = []
+        for (var key in json) {
+            if (json[key].is_league) {
+                leagues.push(json[key].leagueoruser)
+            }
+        }
+
+        //remove leagues no longer present
+        $('#filters .filter').each(function(index) {
+            if (!leagues.includes($(this).text())) {
+                if ($(this).attr('id') != 'selected_filter') {
+                    $(this).remove();
+                }
+            } else {
+                leagues.splice(leagues.indexOf($(this).text()), 1);
+            }
+        })
+
+        // add leagues not already present
+        for (var league in leagues) {
+            $('filters').append("<div class='filter'>"+league+"</div>");
+        }
+    });
+
+    const updateGames = (json, filter) => {
+        filterjson = Object();
+        for (const timestamp in json) {
+            if (json[timestamp].leagueoruser == filter || filter == "All") {
+                filterjson[timestamp] = json[timestamp];
+            }
+        }
 
         if (Object.keys(json).length == 0) {
             $('#footer div').html("No games right now. Why not head over to Discord and start one?");
@@ -17,8 +52,8 @@ $(document).ready(function (){
         }
 
         for (const timestamp in json) {
-            if (!gameslist.includes(timestamp)) { //adds game to list if not there already
-                gameslist.push(timestamp)
+            //adds game to list if not there already
+            if (!grid.children.some((x) => x.timestamp == timestamp)) {
                 for (var slotnum = 0; true; slotnum++) { //this is really a while loop but shh don't tell anyone
                     if (slotnum >= grid.children.length) {
                         for (var i = 0; i < 3; i ++) {
@@ -32,6 +67,7 @@ $(document).ready(function (){
                 };
             };
 
+            //updates game in list
             for (var slotnum = 0; slotnum < grid.children.length; slotnum++) {
                 if (grid.children[slotnum].timestamp == timestamp) {
                     updateGame(grid.children[slotnum], json[timestamp]);
@@ -39,23 +75,24 @@ $(document).ready(function (){
             };
         };
 
+        //replace games that have ended with empty slots
         for (var slotnum = 0; slotnum < grid.children.length; slotnum++) {
-            if (grid.children[slotnum].className == "game" && !(Object.keys(json).includes(grid.children[slotnum].timestamp))) {
+            if (grid.children[slotnum].className == "game" && !Object.keys(json).includes(grid.children[slotnum].timestamp)) {
                 grid.children[slotnum].className = "emptyslot";
                 grid.children[slotnum].innerHTML = "";
             }
         }
 
+        //remove last rows if not needed
         while (grid.children[grid.children.length-1].className == "emptyslot" &&
                grid.children[grid.children.length-2].className == "emptyslot" &&
                grid.children[grid.children.length-3].className == "emptyslot" &&
                grid.children.length > 3) {
-
             for (var i = 0; i < 3; i++) {
                 grid.removeChild(grid.children[grid.children.length-1]);
             }
         }
-    });
+    }
 
     const insertEmpty = (grid) => {
         newBox = document.createElement("DIV");
@@ -103,7 +140,6 @@ $(document).ready(function (){
         $('#updateTarget .update_text').html(gamestate.update_text);
 
         $('#updateTarget .batting').html((gamestate.display_top_of_inning ? gamestate.away_name : gamestate.home_name) + " batting.");
-        console.log(gamestate.leagueoruser)
         $('#updateTarget .leagueoruser').html(gamestate.leagueoruser);
 
         gamediv.id = "";
